3d11b72012fcad51394f657eff953e60a5111a7a
Nehanilajkar <56997283+Nehanilajkar@users.noreply.github.com>
2022-08-16 14:40:26 +0530

    Update higher_lower_game.py

--- higher_lower_game/higher_lower_game.py
+++ higher_lower_game/higher_lower_game.py
@@ -2,26 +2,26 @@ import logo
 from db import data
 import os
 from time import sleep
+import random
 
-i,j=0,1
 play=True
 score=0
 
 print(logo.art)
 
-while play:
-    if data[i]['follower_count']>data[j]['follower_count']:
+while play:  
+    account_a=random.choice(data)
+    account_b=random.choice(data)
+    if account_a['follower_count']>account_b['follower_count']:
         winner='A'
     else:
         winner='B'
-    print(f"Compare A: {data[i]['name']}, a {data[i]['description']}, from {data[i]['country']}")
+    print(f"Compare A: {account_a['name']}, a {account_a['description']}, from {account_a['country']}")
     print(logo.vs)
-    print(f"Against B: {data[j]['name']}, a {data[j]['description']}, from {data[j]['country']}")
+    print(f"Against B: {account_b['name']}, a {account_b['description']}, from {account_b['country']}")
     user_vote=input("Who has more followers? Type 'A' or 'B':")
     if user_vote==winner:
         score+=1
-        i=j
-        j=j+1
         
     else:
         play=False
